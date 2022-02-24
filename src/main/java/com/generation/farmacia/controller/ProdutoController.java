95d3d5eafb0942c1a89321dd4b05cdafeb14af40
Lucas <lucas.orodrigues@hotmail.com>
2022-02-24 18:40:57 -0300

    Farmacia

---
+++ src/main/java/com/generation/farmacia/controller/ProdutoController.java
@@ -0,0 +1,103 @@
+package com.generation.farmacia.controller;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.farmacia.model.Produto;
+import com.generation.farmacia.repository.CategoriaRepository;
+import com.generation.farmacia.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping("/produtos")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoRepository repository;
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAll() {
+		return ResponseEntity.ok(repository.findAll());
+
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> getById(@PathVariable Long id) {
+		return repository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Produto>> getByNome(@PathVariable String nome) {
+		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+	}
+
+	@GetMapping("/laboratorio/{laboratorio}")
+	public ResponseEntity<List<Produto>> getByLaboratorio(@PathVariable String laboratorio) {
+		return ResponseEntity.ok(repository.findAllByLaboratorioContainingIgnoreCase(laboratorio));
+	}
+
+	@GetMapping("/entre/{preco}/{preco1}")
+	public ResponseEntity<List<Produto>> findByEntre(@PathVariable BigDecimal preco, @PathVariable BigDecimal preco1) {
+		return ResponseEntity.ok(repository.findAllByPrecoBetweenOrderByPreco(preco, preco1));
+	}
+
+	@PostMapping
+	public ResponseEntity<Produto> postProduto(@Valid @RequestBody Produto produto) {
+
+		if (categoriaRepository.existsById(produto.getCategoria().getId()))
+			return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
+
+		return ResponseEntity.notFound().build();
+	}
+
+	@PutMapping
+	public ResponseEntity<Produto> putProduto(@RequestBody Produto produto) {
+		if (repository.existsById(produto.getId())) {
+			return categoriaRepository.findById(produto.getCategoria().getId())
+					.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(repository.save(produto)))
+					.orElse(ResponseEntity.notFound().build());
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteProduto(@PathVariable Long id) {
+		Optional<Produto> produto = repository.findById(id);
+
+		if (produto.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+		repository.deleteById(id);
+	}
+
+	@GetMapping("/preco_maior/{preco}")
+	public ResponseEntity<List<Produto>> getPrecoMaiorQue(@PathVariable BigDecimal preco) {
+		return ResponseEntity.ok(repository.findByPrecoGreaterThanOrderByPreco(preco));
+	}
+
+	@GetMapping("/preco_menor/{preco}")
+	public ResponseEntity<List<Produto>> getPrecoMenorQue(@PathVariable BigDecimal preco) {
+		return ResponseEntity.ok(repository.findByPrecoLessThanOrderByPrecoDesc(preco));
+	}
+}
